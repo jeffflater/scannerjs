f92626dfe8a382796e31cc47ea4a8d76544e8150
jeff.flater <jeff.flater@semaphoredoes.com>
2014-07-24 12:12:36 -0400

    develop que to transfer a single image even though file watcher may fire mutiple times

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ var express = require('express'),
 
 var app = express();
 
+var documentModel = function (fileName, image, dateTimeSent, isSent) {
+    this.fileName = fileName;
+    this.image = image;
+    this.dateTimeSent = dateTimeSent;
+    this.isSent = isSent;
+};
+var documentQue = [];
+
 // all environments
 app.set('port', process.env.PORT || 3005);// jshint ignore:line
 app.set('views', path.join(__dirname, 'views'));// jshint ignore:line
@@ -41,7 +49,48 @@ var socketServer = io.listen(server);
 
 //File system watcher...
 fs.watch('c:\\tmp', function(event, targetfile){
-    var file = targetfile.toString();
-    console.log('Sending file: '+file);
-    socketServer.sockets.emit('document', { name: file });
+
+    if (targetfile != null) {
+        console.log('Found Target File!');
+
+        var fileName = targetfile.toString();
+
+        findDocumentByFileName(fileName, function(foundDocument){
+
+            console.log('foundDocument: '+foundDocument);
+
+            //Add document to QUE if NOT found...
+            if (!foundDocument) {
+                var document = new documentModel();
+                document.fileName = fileName;
+                document.image = null; //todo: figure out how to transfer image
+                document.dateTimeSent = new Date();
+                document.isSent = false;
+
+                documentQue.push(document);
+            }
+
+            //Send documents that have not been sent...
+            for (var document in documentQue) {
+                if (documentQue[document].isSent == false) {
+                    socketServer.sockets.emit('document', documentQue[document]);
+                    documentQue[document].isSent = true;
+                }
+            }
+
+        });
+    }
 });
+
+function findDocumentByFileName(fileName, callback) {
+    var foundDocument = false;
+    console.log('documentQue.length: '+documentQue.length);
+
+    for (var document in documentQue) {
+        if (documentQue[document].fileName === fileName) {
+            foundDocument = true;
+            break;
+        }
+    }
+    callback(foundDocument);
+}
